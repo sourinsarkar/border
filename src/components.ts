31f7c666d8b6b2a9f1d1e222ad5367e0a6eb0c92
Sourin <iamsourinsarkar@gmail.com>
2024-10-04 12:59:09 +0530

    Configuration issues'

---
+++ src/components.ts
@@ -0,0 +1,2 @@
+export { DashedBox } from "./components/DashedBox"
+export { DashedLine } from "./components/DashedLine"
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/components.ts',
+      name: 'YourLibraryName',
+      fileName: (format) => `your-library-name.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
